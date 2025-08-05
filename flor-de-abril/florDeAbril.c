e2790c4f8d4b3990941404f4644ad166d6a16ecf
Emile Noemi Alves da Silva <enas@ic.ufal.br>
2025-08-05 12:47:18 -0300

    Create florDeAbril.c

---
+++ flor-de-abril/florDeAbril.c
@@ -0,0 +1,65 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+void init(){
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Haste amarela
+    glColor3f(1.0, 1.0, 0.0); 
+    glBegin(GL_LINES);
+        glVertex2i(250, 100);
+        glVertex2i(250, 230);
+    glEnd();
+
+    // Pás do catavento
+    // Vermelha (direita)
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2i(250, 250);
+        glVertex2i(290, 230);
+        glVertex2i(290, 270);
+    glEnd();
+
+    // Verde (topo)
+    glColor3f(0.0, 1.0, 0.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2i(250, 250);
+        glVertex2i(230, 290);
+        glVertex2i(270, 290);
+    glEnd();
+
+    // Azul (esquerda)
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2i(250, 250);
+        glVertex2i(210, 230);
+        glVertex2i(210, 270);
+    glEnd();
+
+    // Branca (base)
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2i(250, 250);
+        glVertex2i(230, 210);
+        glVertex2i(270, 210);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(200, 100);
+    glutCreateWindow("Catavento Colorido");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
