1bad15345b439cfe93a0cc24b8fe50880ec471ef
Emile Noemi Alves da Silva <enas@ic.ufal.br>
2025-08-05 22:01:13 -0300

    Update florDeAbril.c

--- flor-de-abril/florDeAbril.c
+++ flor-de-abril/florDeAbril.c
@@ -1,4 +1,3 @@
-#include <windows.h>
 #include <GL/glut.h>
 
 void init(){
@@ -9,10 +8,17 @@ void init(){
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(1.0, 1.0, 0.0); 
+    glColor3f(1.0, 1.0, 0); 
     glBegin(GL_LINES);
         glVertex2i(250, 100);
         glVertex2i(250, 230);
+    glEnd();
+
+     glColor3f(0.0, 1.0, 0.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2i(250, 250);
+        glVertex2i(230, 290);
+        glVertex2i(270, 290);
     glEnd();
 
     glColor3f(1.0, 0.0, 0.0);
@@ -22,11 +28,11 @@ void display(){
         glVertex2i(290, 270);
     glEnd();
 
-    glColor3f(0.0, 1.0, 0.0);
+     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_TRIANGLES);
         glVertex2i(250, 250);
-        glVertex2i(230, 290);
-        glVertex2i(270, 290);
+        glVertex2i(230, 210);
+        glVertex2i(270, 210);
     glEnd();
 
     glColor3f(0.0, 0.0, 1.0);
@@ -36,12 +42,6 @@ void display(){
         glVertex2i(210, 270);
     glEnd();
 
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_TRIANGLES);
-        glVertex2i(250, 250);
-        glVertex2i(230, 210);
-        glVertex2i(270, 210);
-    glEnd();
 
     glFlush();
 }
@@ -50,10 +50,11 @@ int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
-    glutInitWindowPosition(200, 100);
-    glutCreateWindow("Catavento Colorido");
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("Flor de Abril");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
+
